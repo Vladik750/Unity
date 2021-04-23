c5439ed0cae56cfc9aff2449c2d9508fbed0bc93
Vlad Zhuk <zhuk.vlad2000@gmail.com>
2021-04-23 17:04:10 +0300

    Door can open now

---
+++ Doors/Assets/Scripts/fif.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fif : MonoBehaviour
+{
+
+    private Rigidbody doorRb;
+    private bool isOpened = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorRb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if(!isOpened)
+        {
+            for (int i = 0; i <= 90; i++)
+            {
+                transform.Rotate(new Vector3(0, 1, 0), -1);
+
+            }
+            transform.position += new Vector3(-1.3f,0,0);
+            isOpened = true;
+        }
+        else
+        {
+            for (int i = 90; i >= 0; i--)
+            {
+                transform.Rotate(new Vector3(0, 1, 0), 1);
+
+            }
+            transform.position += new Vector3(1.3f, 0, 0);
+            isOpened = false;
+        }
+        
+    }
+}
